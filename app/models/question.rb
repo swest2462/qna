4b847891887aec091f79ea0552a75f1854c25b5e
Aleksandr Vest <swest2462@gmail.com>
2016-08-17 23:58:09 +0300

    fixed tabs

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   validates :title, length: { minimum: 3, maximum: 150 }, presence: true
- 	validates :body, length: { minimum: 5, maximum: 240 }, presence: true
+ 	validates :body, length: { minimum: 5 }, presence: true
 end
