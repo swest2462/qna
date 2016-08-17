74cfe6427adccaeecc624f8349b650fba1984302
Aleksandr Vest <swest2462@gmail.com>
2016-08-17 23:31:35 +0300

    add index

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
-  validates :title, :body, presence: true
+  validates :title, length: { minimum: 3, maximum: 150 }, presence: true
+ 	validates :body, length: { minimum: 5, maximum: 240 } presence: true
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :answer do
     body "MyText"
     question ""
+    association :question
   end
 end
