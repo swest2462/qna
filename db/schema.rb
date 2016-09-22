bb5312e50ee63ad7b3ca7dfa1b6cba1ee51c3fa9
Aleksandr Vest <swest2462@gmail.com>
2016-09-22 16:49:16 +0300

    tests for question and answer controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817192709) do
+ActiveRecord::Schema.define(version: 20160922133220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,17 @@ ActiveRecord::Schema.define(version: 20160817192709) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.integer  "answer_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "users", ["answer_id"], name: "index_users_on_answer_id", using: :btree
+  add_index "users", ["question_id"], name: "index_users_on_question_id", using: :btree
+
   add_foreign_key "answers", "questions"
+  add_foreign_key "users", "answers"
+  add_foreign_key "users", "questions"
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    answer nil
+    question nil
+  end
+end
